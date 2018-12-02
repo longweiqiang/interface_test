c76fd3552af19dd7c7f4cf1ec0fad03ad5cf23e6
longweiqiang <573925242@qq.com>
2018-12-02 16:08:33 +0800

    第一次提交

---
+++ primitive_datatypes_and_operators.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+# @Time       : 2018/12/2 15:59
+# @Author     : Weiqiang.long
+# @File       : primitive_datatypes_and_operators.py
+# @Software   : PyCharm
+# @Description: 
+# @TODO       :
+
+# '''XXX''' 单行注释
+
+"""
+    使用3个双引号
+    来定义多行文本
+    多行文本一般被用来
+    做注释用了
+"""
+
+# 这是数字
+3 # >=3
+
+# 简单的算术
+print(1 + 1)    # 2
+print(8 - 1)    # 7
+print(10 * 2)   # 20
+print(35 / 5)   # 7.0
+
+print(5 / 2)
+
+
+
+
+
+
+
+
+
+
